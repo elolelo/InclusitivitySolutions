d6dfbac9cf7cf237e3a952ee189e455bee5d0edc
Nompumelelo <32453496+elolelo@users.noreply.github.com>
2019-02-25 23:00:01 +0200

    Testing the hand

---
+++ HandTest1.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HandTest1 {
+	Hand m_hand;
+	
+	@Before
+	public void setUp() throws Exception {
+		///super.setUp();
+		m_hand = new Hand();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
