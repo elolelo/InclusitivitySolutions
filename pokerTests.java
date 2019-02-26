20e6e0fd2bf78c57aea7f232f0b31ffea852bd54
Nompumelelo <32453496+elolelo@users.noreply.github.com>
2019-02-26 09:30:30 +0200

    Testing the 4 of a kind

--- pokerTests.java
+++ pokerTests.java
@@ -1,14 +1,64 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
+//checks the 4 of a kind
+public class FourOfAKindCheckerTest {
+	
+	private RankChecker checker;
+	
+	@Before
+	public void before() {
+		this.checker = new FourOfAKindChecker();
+	}
+	
+	@Test
+	public void handShouldBeRankedAsFourOfAKind() {
+		for (CardValue value : CardValue.values()) {
+			//The different Value:
+			CardValue different = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value)).findAny().get();
+			Card other = new Card(CardSuit.HEARTS, different);
+			
+			Card first = new Card(CardSuit.CLUBS, value);
+			Card second = new Card(CardSuit.HEARTS, value);
+			Card third = new Card(CardSuit.SPADES, value);
+			Card fourth = new Card(CardSuit.DIAMONDS, value);
+			
+			Set<Card> set = new HashSet<>(Arrays.asList(first, second, third, fourth, other));
+			Cards cards = new Cards(set);
+	
+			boolean matches = checker.matches(cards);
+			
+			Assert.assertTrue(matches);
+		}
+	}
+	
+	@Test
+	public void handShouldNotBeRankedAsFourOfAKind() {
+		for (CardValue value : CardValue.values()) {
+			//The different Value:
+			CardValue different = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value)).findAny().get();
+			Card other = new Card(CardSuit.HEARTS, different);
+			Card first = new Card(CardSuit.CLUBS, different);
+			
+			Card second = new Card(CardSuit.HEARTS, value);
+			Card third = new Card(CardSuit.SPADES, value);
+			Card fourth = new Card(CardSuit.DIAMONDS, value);
+			
+			Set<Card> set = new HashSet<>(Arrays.asList(first, second, third, fourth, other));
+			Cards cards = new Cards(set);
+	
+			boolean matches = checker.matches(cards);
+			
+			Assert.assertFalse(matches);
+		}
+	}
+	
+}
 
-
-// this checks the flush method/class
+/* this checks the flush class
 
 public class FlushCheckerTest {
 	
-	private RankChecker checker;
+	private RankChecker checker;  
 	
 	@Before
 	public void before() {
@@ -55,4 +105,7 @@ public class FlushCheckerTest {
 	}
 	
 }
+
+*/
+
 
