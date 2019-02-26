8e07692afb63eb5b7cc2935951586023fc3f9b7a
Nompumelelo <32453496+elolelo@users.noreply.github.com>
2019-02-26 10:47:40 +0200

    Ranks as enums

---
+++ Rank.java
@@ -0,0 +1,87 @@
+
+public enum Rank {
+
+	ROYAL_FLUSH("Royal Flush", 10) {
+		@Override
+		public RankChecker getChecker() {
+			return new RoyalFlushChecker();
+		}
+	},
+	STRAIGHT_FLUSH("Straight Flush", 9) {
+		@Override
+		public RankChecker getChecker() {
+			return null;
+		}
+	},
+	FOUR_OF_A_KIND("Four of a Kind", 8) {
+		@Override
+		public RankChecker getChecker() {
+			return null;
+		}
+	},
+	FULL_HOUSE("Full House", 7) {
+		@Override
+		public RankChecker getChecker() {
+			return null;
+		}
+	},
+	FLUSH("Flush", 6) {
+		@Override
+		public RankChecker getChecker() {
+			return null;
+		}
+	},
+	STRAIGHT("Straight", 5) {
+		@Override
+		public RankChecker getChecker() {
+			return null;
+		}
+	},
+	THREE_OF_A_KIND("Three of a Kind", 4) {
+		@Override
+		public RankChecker getChecker() {
+			return null;
+		}
+	},
+	TWO_PAIRS("Tow Pairs", 3) {
+		@Override
+		public RankChecker getChecker() {
+			return null;
+		}
+	},
+	ONE_PAIR("One Pair", 2) {
+		@Override
+		public RankChecker getChecker() {
+			return null;
+		}
+	},
+	HIGH_CARD("High Card", 1) {
+		@Override
+		public RankChecker getChecker() {
+			return null;
+		}
+	};
+	
+	private final String name;
+	private final int strength;
+
+	private Rank(String name, int strength) {
+		this.name = name;
+		this.strength = strength;
+	}
+	
+	public abstract RankChecker getChecker();
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public int getStrength() {
+		return strength;
+	}
+	
+}
