a1c92031fa8ef84ead64862f279e2a916a27787d
Nompumelelo <32453496+elolelo@users.noreply.github.com>
2019-02-26 12:46:08 +0200

    Testing 3 of a kind

--- pokerTests.java
+++ pokerTests.java
@@ -1,8 +1,67 @@
 import java.util.*;
 
+//3 of a kind
+public class ThreeOfAKindCheckerTest {
+	
+	private RankChecker checker;
+	
+	@Before
+	public void before() {
+		this.checker = new ThreeOfAKindChecker();
+	}
+	
+	@Test
+	public void handShouldBeRankedAsThreeOfAKind() {
+		for (CardValue value : CardValue.values()) {
+			//The differents Values:
+			CardValue firstDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value)).findAny().get();
+			CardValue secondDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value) && !v.equals(firstDifferent)).findAny().get();
+			Card other = new Card(CardSuit.HEARTS, firstDifferent);
+			Card secondOther = new Card(CardSuit.CLUBS, secondDifferent);
+			
+			Card first = new Card(CardSuit.HEARTS, value);
+			Card second = new Card(CardSuit.SPADES, value);
+			Card third = new Card(CardSuit.DIAMONDS, value);
+			
+			Set<Card> set = new HashSet<>(Arrays.asList(first, second, third, other, secondOther));
+			Cards cards = new Cards(set);
+	
+			boolean matches = checker.matches(cards);
+			
+			Assert.assertTrue(matches);
+		}
+	}
+	
+	@Test
+	public void handShouldNotBeRankedAsThreeOfAKind() {
+		for (CardValue value : CardValue.values()) {
+			//The differents Values:
+			CardValue firstDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value)).findAny().get();
+			CardValue secondDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value) && !v.equals(firstDifferent)).findAny().get();
+			CardValue thirdDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value) && !v.equals(firstDifferent) && !v.equals(secondDifferent)).findAny().get();
+			Card other = new Card(CardSuit.HEARTS, firstDifferent);
+			Card secondOther = new Card(CardSuit.CLUBS, secondDifferent);
+			Card thirdOther = new Card(CardSuit.SPADES, thirdDifferent);
+			
+			Card first = new Card(CardSuit.HEARTS, value);
+			Card second = new Card(CardSuit.SPADES, value);
+			
+			Set<Card> set = new HashSet<>(Arrays.asList(first, second, other, secondOther, thirdOther));
+			Cards cards = new Cards(set);
+	
+			boolean matches = checker.matches(cards);
+			
+			Assert.assertFalse(matches);
+		}
+	}
+	
+}
 
 
-// checks for high card:
+
+
+
+/* checks for high card:
 
 public class HighCardCheckerTest {
 	
@@ -155,8 +214,8 @@ public class FourOfAKindCheckerTest {
 	
 }
 
- this checks the flush class
 
+this checks the flush class
 public class FlushCheckerTest {
 	
 	private RankChecker checker;  
